256c9ee76c455f04c8769c0cb77b738fc9cc90ec
elelele <hitrovaeleonora@gmail.com>
2020-06-02 20:27:03 +0300

    Email sender finished

--- src/context/stepState.js
+++ src/context/stepState.js
@@ -1,10 +1,10 @@
-import {StepContext} from './stepContext';
-import React, {useReducer} from 'react';
-import {stepReducer} from './stepReducer';
-import {FINISH_FIRST, FINISH_SECOND, FINISH_THIRD, FINISH_FOURTH, FINISH_FIFTH} from './types';
-import {sendEmail} from '../helpers/send-email'
+import { StepContext } from './stepContext';
+import React, { useReducer } from 'react';
+import { stepReducer } from './stepReducer';
+import { FINISH_FIRST, FINISH_SECOND, FINISH_THIRD, FINISH_FOURTH, FINISH_FIFTH } from './types';
+import { sendEmail } from '../helpers/send-email'
 
-export const StepState = ({children}) => {
+export const StepState = ({ children }) => {
   const initialState = {
     step1: {},
     step2: {},
@@ -16,7 +16,6 @@ export const StepState = ({children}) => {
   const [state, dispatch] = useReducer(stepReducer, initialState);
 
   const finishFirst = (localState) => {
-    console.log(localState);
     dispatch({
       type: FINISH_FIRST,
       payload: localState,
@@ -24,7 +23,6 @@ export const StepState = ({children}) => {
   };
 
   const finishSecond = (localState) => {
-    console.log(localState);
     dispatch({
       type: FINISH_SECOND,
       payload: localState,
@@ -32,7 +30,6 @@ export const StepState = ({children}) => {
   };
 
   const finishThird = (localState) => {
-    console.log(localState);
     dispatch({
       type: FINISH_THIRD,
       payload: localState,
@@ -40,7 +37,6 @@ export const StepState = ({children}) => {
   };
 
   const finishFourth = (localState) => {
-    console.log(localState);
     dispatch({
       type: FINISH_FOURTH,
       payload: localState,
@@ -48,15 +44,14 @@ export const StepState = ({children}) => {
   };
 
   const finishFifth = (localState) => {
-    console.log(localState);
     dispatch({
       type: FINISH_FIFTH,
       payload: localState,
     });
     sendEmail(
       'abc@abc.abc',
-      'Test task',
-      '',
+      'Test Mail',
+      state,
     ).then(() => {
       console.log('Your message was successfully sent!');
     });
@@ -65,8 +60,8 @@ export const StepState = ({children}) => {
 
   return (
 
-        <StepContext.Provider value={{finishFirst, finishSecond, finishThird, finishFourth, finishFifth, stepperState: state}}>
-            {children}
-        </StepContext.Provider>
+    <StepContext.Provider value={{ finishFirst, finishSecond, finishThird, finishFourth, finishFifth, stepperState: state }}>
+      {children}
+    </StepContext.Provider>
   );
 };
